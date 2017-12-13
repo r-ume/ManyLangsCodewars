be88107d83bcf60fefc7276325235867cd35e77a
r-ume <baymax.blackandwhite@gmail.com>
2017-12-13 14:40:30 +0900

    added removePuckHiccup.js

---
+++ js/7-kyu/removePuckHiccup.js
@@ -0,0 +1,18 @@
+// Drunk Wise man
+
+// Fortunately last weekend, I met an utterly drunk old man. He was too drunk to be aggressive towards me.
+// He was letting everything what he held out, from both his mind and his stomach.
+// Although i was a bit uncomfortable, the old man's broken wisdom words caught my attention. But his talk was not continuous as it was frequently interrupted by an involuntary contractions 'puke' and 'hiccup' . Now i am hiring you to clean up his 'puke' and 'hiccup' and tell me the old man's wisdom words.
+// Because drunk man also needs to take a pause and take a deep breath, you have to remove those pauses (redundant/unnecessary spaces).
+
+pry = require('pryjs');
+
+String.prototype.removeHiccupPuke = function(){
+  // My Answer
+  // return this.replace(/puke /g , '').replace(/hiccup /g, '').replace( /\s\s+/g, ' ' );
+
+  // Best Answer
+  return this.replace(/puke|hiccup/g, '').trim().replace(/ {2,}/g, ' ')
+};
+
+console.log('puke All\'s well hiccup     that ends hiccup well puke'.removeHiccupPuke());
