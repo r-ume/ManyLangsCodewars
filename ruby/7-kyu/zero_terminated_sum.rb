822ed4fce9f41947817980110923da0798797aed
r-ume <baymax.blackandwhite@gmail.com>
2017-12-14 14:03:19 +0900

    added zero_terminated_sum.rb

---
+++ ruby/7-kyu/zero_terminated_sum.rb
@@ -0,0 +1,13 @@
+# Zero terminated sum
+
+require 'rubygems'
+require 'pry'
+
+class String
+  def zero_terminated_sum
+    binding.pry
+    self.split('0').map(&:chars).reject(&:empty?).map { |element| element.map(&:to_i).sum }.sort.reverse.first.to_i
+  end
+end
+
+"72102450111111090".zero_terminated_sum
