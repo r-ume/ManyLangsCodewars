f1c2210ce8a37c9f69341e48e7c7239c78cb7793
r-ume <baymax.blackandwhite@gmail.com>
2017-12-26 15:58:18 +0900

    added opposite.go

---
+++ go/8-kyu/opposite.go
@@ -0,0 +1,18 @@
+// Opposite Number
+
+// Very simple, given a number, find its opposite.
+
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+  fmt.Println(Opposite(-1))
+
+}
+
+func Opposite(value int) int {
+  return -value
+}
