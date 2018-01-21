4d1358b291f935672301b1f44040936268c3ff9f
r-ume <baymax.blackandwhite@gmail.com>
2018-01-21 18:12:33 +0900

    added multiply_of_index.go

---
+++ go/8-kyu/multiply_of_index.go
@@ -0,0 +1,36 @@
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+// Some cases:
+
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+// [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+  ints := []int{22, -6, 32, 82, 9, 25}
+  fmt.Println(MultipleOfIndex(ints))
+}
+
+func MultipleOfIndex(ints []int) []int{
+  var results []int
+
+  for index, element := range ints {
+    if index == 0 {
+      continue
+    }
+    if element % index == 0 {
+      results = append(results, element)
+    }
+  }
+
+  return results
+}
