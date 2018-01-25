eabd3b49e687d9986336d66c738755a49e402437
r-ume <baymax.blackandwhite@gmail.com>
2018-01-25 13:58:06 +0900

    added high_score_table.rb

---
+++ ruby/6-kyu/high_score_table.rb
@@ -0,0 +1,38 @@
+# You just got done writing a function that calculates the player's final score for your new game, "Flight of the cockatoo".
+# Now all you need is a high score table that can be updated with the player's final scores. With such a feature, the player will be motivated to try to beat his previous scores, and hopefully, never stop playing your game.
+# The high score table will start out empty. A limit to the size of the table will be specified upon creation of the table.
+#
+# Here's an example of the expected behavior of the high score table :
+# highScoreTable = HighScoreTable(3)
+# highScoreTable.scores == [] # evaluates to True
+# highScoreTable.update(10)
+# highScoreTable.scores == [10]
+# highScoreTable.update(8)
+# highScoreTable.update(12)
+# highScoreTable.update(5)
+# highScoreTable.update(10)
+# highScoreTable.scores == [12, 10, 10]
+# highScoreTable.reset()
+# highScoreTable.scores == []
+# # And so on...
+
+class HighScoreTable
+  def initialize(size)
+    @size = size
+    @score_list = []
+  end
+
+  def scores
+    @score_list = @score_list.sort.reverse
+  end
+
+  def update(score)
+    return @score_list << score if @score_list.length < @size
+    @score_list = @score_list.sort.reverse
+    @score_list[@size - 1] = score if @score_list[@size - 1] < score
+  end
+
+  def reset
+    @score_list = []
+  end
+end
